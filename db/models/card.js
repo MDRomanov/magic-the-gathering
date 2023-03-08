a015a7b19af497c55720787563ceaacba0de1ecf
Max Romanov <maxromanov@MacBook-Pro-Maksim.local>
2023-03-08 11:42:26 +0300

    database and dependancies complete

---
+++ db/models/card.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Card extends Model {
+    static associate({ User }) {
+      this.belongsTo(User, { foreignKey: 'userId' });
+    }
+  }
+  Card.init(
+    {
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      img: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      price: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      newOrOld: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      userCity: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Card',
+    },
+  );
+  return Card;
+};
