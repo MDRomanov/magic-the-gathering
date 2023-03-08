e508add619e206e1601ba1060c576ee69219c360
Dmitriy-Ivanof <dmitrii_i@bk.ru>
2023-03-08 19:40:08 +0300

    create routers auth-n and create react components

---
+++ public/scripts/autorisationScript.js
@@ -0,0 +1,51 @@
+const formReg = document.querySelector("#formReg");
+const formLog = document.querySelector("#formLog");
+console.log(formReg);
+if (formReg) {
+  formReg.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const { name, email, password, password2, action, method } = e.target;
+    console.log(name);
+    const res = await fetch(action, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        password2: password2.value,
+      }),
+    });
+    const data = await res.json();
+    if (data.message === "ok") {
+      window.location.assign("/magicard");
+    } else {
+      document.querySelector(".error").innerHTML = data.message;
+    }
+  });
+}
+
+if (formLog) {
+  formLog.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const { email, password, action, method } = e.target;
+    const res = await fetch(action, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+      }),
+    });
+    const data = await res.json();
+    if (data.message === "ok") {
+      window.location.assign("/magicard");
+    } else {
+      document.querySelector(".error").innerHTML = data.message;
+    }
+  });
+}
