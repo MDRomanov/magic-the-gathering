7705338c2c72d190712eb45fc7b65d924011cf29
Max Romanov <maxromanov@MacBook-Pro-Maksim.local>
2023-03-09 16:40:10 +0300

    changse for QOL

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -5,7 +5,7 @@ const Layout = require('./Layout');
 
 module.exports = function CardList({ cards, user }) {
   return (
-    <Layout>
+    <Layout user={user}>
       <div className="div-header">
         <h1 className="card-list">Доступные карты</h1>
       </div>
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -4,10 +4,6 @@ const Main = require('../components/Main');
 
 router.get('/', (req, res) => {
   try {
-    res.cookie('home', 'Welcome to our website!', {
-      maxAge: 30000,
-      httpOnly: true,
-    });
     res.renderComponent(Main, { title: 'Main' });
   } catch (error) {
     res.status(500).json(error.message);
